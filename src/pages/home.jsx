628de1b197cb3db729baec88d4126f0397936531
Glitch (glitch-hello-react) <none>
2023-04-07 07:49:55 +0000

    🎇🐾 Checkpoint ./src/pages/home.jsx:3125264/3665 ./src/styles/styles.css:3125264/252

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,34 @@
 import * as React from "react";
 
-export default function Home() {
-  const n = 11
-  const radius = 250
-  return <svg width={600} height={600} viewBox="-300 -300 600 600">
-    {[...Array(n).keys()].map(i => {
-      const x = radius * Math.cos(i * 2 * Math.PI / n)
-      const y = radius * Math.sin(i * 2 * Math.PI / n)
+export default function DiagramPage() {
+  return <div className="DiagramPage">
+    <Sidebar />
+    <Diagram />
+  </div>;
+}
 
-      return <g transform={`translate(${x}, ${y})`}>
-        <circle stroke="grey" strokeWidth={1} fill="none" r={10}></circle>
-        <text textAnchor="middle" dominantBaseline="middle">{i+1}</text>
-      </g>
-    })}
-  </svg>
+function Sidebar() {
+  return <section />
+}
+
+function Diagram() {
+  const n = 12;
+  const radius = 250;
+  return (
+    <svg width={600} height={600} viewBox="-300 -300 600 600">
+      {[...Array(n).keys()].map((i) => {
+        const x = radius * Math.cos((i * 2 * Math.PI) / n);
+        const y = radius * Math.sin((i * 2 * Math.PI) / n);
+
+        return (
+          <g transform={`translate(${x}, ${y})`}>
+            <circle stroke="grey" strokeWidth={1} fill="none" r={20}></circle>
+            <text textAnchor="middle" dominantBaseline="middle">
+              {i + 1}
+            </text>
+          </g>
+        );
+      })}
+    </svg>
+  );
 }
