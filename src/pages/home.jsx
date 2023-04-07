0faf0402d3f7862fc35b885c7c0bf8a6caaa1625
Glitch (glitch-hello-react) <none>
2023-04-07 09:11:00 +0000

    😂📖 Checkpoint ./src/pages/home.jsx:3125264/4576

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,18 @@
 import * as React from "react";
-import { parseCycleNotation } from "../util"
+import { parseCycleNotation } from "../util";
 
 export default function DiagramPage() {
   const [numElements, setNumElements] = React.useState(6);
-  const [generators, setGenerators] = React.useState("(1 2 3)\n(4 5 6)")
+  const [generators, setGenerators] = React.useState("(1 2 3)\n(4 5 6)");
   return (
     <div className="DiagramPage">
-      <Sidebar numElements={numElements} setNumElements={setNumElements} generators={generators} setGenerators={setGenerators} />
-      <Diagram numElements={numElements} />
+      <Sidebar
+        numElements={numElements}
+        setNumElements={setNumElements}
+        generators={generators}
+        setGenerators={setGenerators}
+      />
+      <Diagram numElements={numElements} generators={parseCycleNotation(generators)} />
     </div>
   );
 }
@@ -26,14 +31,17 @@ function Sidebar({ numElements, setNumElements, generators, setGenerators }) {
       </label>
       <label>
         <div>Generators (in cycle notation, one generator per line)</div>
-        <textarea value={generators} onChange={e => setGenerators(e.target.value)}/>
+        <textarea
+          value={generators}
+          onChange={(e) => setGenerators(e.target.value)}
+        />
         {JSON.stringify(parseCycleNotation(generators))}
       </label>
     </section>
   );
 }
 
-function Diagram({ numElements }) {
+function Diagram({ numElements, generators }) {
   const n = numElements;
   const radius = 250;
   return (
@@ -44,9 +52,7 @@ function Diagram({ numElements }) {
       viewBox="-300 -300 600 600"
     >
       {[...Array(n).keys()].map((i) => {
-        const x = radius * Math.cos((i * 2 * Math.PI) / n - Math.PI/2);
-        const y = radius * Math.sin((i * 2 * Math.PI) / n - Math.PI/2);
-
+        const [x, y] = getCoordinates(i, n, radius)
         return (
           <g key={i} transform={`translate(${x}, ${y})`}>
             <circle stroke="grey" strokeWidth={1} fill="none" r={20}></circle>
@@ -58,4 +64,10 @@ function Diagram({ numElements }) {
       })}
     </svg>
   );
+}
+
+function getCoordinates(i, n, radius) {
+  const x = radius * Math.cos((i * 2 * Math.PI) / n - Math.PI / 2);
+  const y = radius * Math.sin((i * 2 * Math.PI) / n - Math.PI / 2);
+  return [x, y];
 }
