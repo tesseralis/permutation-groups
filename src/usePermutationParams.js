f5c44b5c61e93c4ee361941f62b4ec532ab4e78e
Glitch (glitch-hello-react) <none>
2023-04-28 06:21:20 +0000

    🔔🚇 Checkpoint ./src/usePermutationParams.js:3125264/8129

---
+++ src/usePermutationParams.js
@@ -0,0 +1,36 @@
+import useSearchParams from "./useSearchParams";
+import { parseGroupName } from "./groups";
+
+import { generatorsToString } from "./util";
+
+export default function usePermutationParams() {
+  const [params, setSearchParams] = useSearchParams();
+  const generators = getGenerators(params);
+
+  const setGenerators = (generators) => {
+    setSearchParams({ generators });
+  };
+
+  const setGroup = (name) => {
+    setSearchParams({ group: name });
+  };
+  return { generators };
+}
+
+const DEFAULT_GENERATORS = "(1 2 3)\n(4 5 6)\n(1 6)(3 4)";
+
+// Get the generators string from the search parameters
+function getGenerators(params = {}) {
+  if (params.group) {
+    const group = parseGroupName(params.group);
+    // parse the family name and return the generators of the corresponding group
+    if (group) {
+      return generatorsToString(group);
+    }
+  }
+  if (params.generators) {
+    return params.generators;
+  }
+
+  return DEFAULT_GENERATORS;
+}
