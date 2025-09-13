2938191cec6d7ce2ef73a83428fe199223f349e0
Glitch (glitch-hello-react) <none>
2025-09-13 14:25:09 +0200

    Update build directory.

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: '/permutation-groups/',
-  build: {
-    outDir: "build"
-  },
   server: {
     host:"0.0.0.0",
     port:3000,
