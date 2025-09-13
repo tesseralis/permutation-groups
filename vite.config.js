9f80e13ed74d942c9ea35419a0bd2d81d182d683
Glitch (glitch-hello-react) <none>
2025-09-13 14:32:07 +0200

    Get rid of weird server specifications.

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host:"0.0.0.0",
-    port:3000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443 // Run the websocket server on the SSL port
-    }
-  }
 });
