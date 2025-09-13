a30274df2bf97d3ad2539e03b454901f09dd1fe1
Glitch (glitch-hello-react) <none>
2025-09-13 14:28:53 +0200

    Fix base.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/permutation-groups/',
   server: {
     host:"0.0.0.0",
     port:3000,
